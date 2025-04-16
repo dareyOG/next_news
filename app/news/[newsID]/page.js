7e53193584c078f47733abc3d5f41ea30f89a430
Oludire Damilare <oludire@gmail.com>
2025-04-16 01:59:24 +0100

    ✅handle not-found errors and render not-found pages

--- app/news/[newsID]/page.js
+++ app/news/[newsID]/page.js
@@ -1,8 +1,11 @@
 import { DUMMY_NEWS } from "@/dummyNews"
+import { notFound } from "next/navigation"
 
 function NewsDetailPage({ params }) {
     const news_slug = params.newsID
     const news = DUMMY_NEWS.find(news => news.slug === news_slug)
+
+    if (!news) notFound()
 
     return (
         <article className="news-article">
